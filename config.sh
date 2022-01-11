91f4644f5682b0219c1dc34907771f0a37989e71
Ao Li <leeleo3x@gmail.com>
2022-01-10 19:02:23 -0600

    update.

---
+++ config.sh
@@ -0,0 +1,13 @@
+sudo useradd mosipuser
+echo -e "mosippwd\nmosippwd" | sudo passwd mosipuser
+sudo usermod -aG wheel mosipuser
+echo "mosipuser ALL=(ALL)  ALL" | sudo EDITOR='tee -a' visudo
+echo "%mosipuser  ALL=(ALL)  NOPASSWD:ALL" | sudo EDITOR='tee -a' visudo
+
+
+sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum install -y ansible
+sudo yum install -y git
+sudo yum install -y nginx-mod-stream 
+
+
---
+++ readme.md
@@ -0,0 +1,15 @@
+Mosip 1.1.5.3 commit 42efbbf5c8e4cf3018ca73f22494bdf7a42a5848
+
+- Code changes:
+  - roles/ssl/letsencrypt/defaults/main.yml:`1.22.0`
+  - k8scluster/cni/templates/flannel.yml.j2: `apiVersion: rbac.authorization.k8s.io/v1`
+
+- Missing Packages:
+  - nginx-mod-stream
+
+
+- Configuration updates:
+  - Add user `nfsnobody`
+  - Enable SELinux
+
+
