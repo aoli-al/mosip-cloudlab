fa7a848a8c1784159b8bb06368eb13c38d9a0469
Ao Li <leeleo3x@gmail.com>
2022-01-12 10:32:24 -0600

    update.

--- config.sh
+++ config.sh
@@ -8,6 +8,7 @@ echo "%mosipuser  ALL=(ALL)  NOPASSWD:ALL" | sudo EDITOR='tee -a' visudo
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum install -y ansible
 sudo yum install -y git
-sudo yum install -y nginx-mod-stream 
+sudo yum install -y nginx-mod-stream htop byobu
+sudo useradd nfsnobody
 
 
