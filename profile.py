08866c88e4023f2ce52c17d14a7dc174b88cb9b4
Ao Li <leeleo3x@gmail.com>
2022-01-25 09:24:47 -0600

    update.

---
+++ profile.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Deploy Mosip in Cloudlab"""
+
+import geni.portal as portal
+import geni.rspec.pg as pg
+import geni.rspec.emulab as emulab
+
+# Create a portal object,
+pc = portal.Context()
+
+# Create a Request object to start building the RSpec.
+request = pc.makeRequestRSpec()
+
+CENTOS_ARN = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+interfaces = []
+
+node = request.RawPC("console")
+node.disk_image = CENTOS_ARN
+node.hardware_type = "d430"
+interfaces.append(node.addInterface('interface-0'))
+
+node = request.XenVM("mzmaster")
+node.disk_image = CENTOS_ARN
+node.ram = 8 * 1024
+node.cores = 4
+node.disk = 64
+interfaces.append(node.addInterface('interface-1'))
+
+for i in range(9):
+    node = request.XenVM("mzworker" + str(i))
+    node.disk_image = CENTOS_ARN
+    node.ram = 16 * 1024
+    node.cores = 4
+    node.disk = 64
+    interfaces.append(node.addInterface('interface-' + str(i + 2)))
+
+node = request.XenVM("dmzmaster")
+node.disk_image = CENTOS_ARN
+node.ram = 8 * 1024
+node.cores = 4
+node.disk = 64
+interfaces.append(node.addInterface('interface-11'))
+
+node = request.XenVM("dmzworker")
+node.disk_image = CENTOS_ARN
+node.ram = 16 * 1024
+node.cores = 4
+node.disk = 64
+interfaces.append(node.addInterface('interface-12'))
+
+link = request.Link('link-0')
+link.Site('undefined')
+for interface in interfaces:
+    link.addInterface(interface)
+
+pc.printRequestRSpec(request)
