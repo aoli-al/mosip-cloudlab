5eb5e2dac67b9ae2f2848e59cb928b3700ff2dc6
Ao Li <leeleo3x@gmail.com>
2022-03-23 13:50:55 -0400

    update.

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@ chmod 600 ~/.ssh/id_rsa
 ssh-keygen -f ~/.ssh/id_rsa -y > ~/.ssh/id_rsa.pub
 
 
-sudo patch -u /etc/yum.repos.d/kubernetes.repo -i kube.patch
+sudo sed 's/repo_gpgcheck=1/repo_gpgcheck=0/g' /etc/yum.repos.d/kubernetes.repo
 sudo useradd mosipuser
 echo -e "alcmw,m\nalcmw,m" | sudo passwd mosipuser
 sudo usermod -aG wheel mosipuser
